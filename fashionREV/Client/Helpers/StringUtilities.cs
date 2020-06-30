419ed772bc7e493fbcfda4c6c8e3e0317e278f30
MEHEDI <me.mehedi@hotmail.com>
2020-06-30 14:28:37 +0600

    Add project files.

---
+++ fashionREV/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fashionREV.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string Name(string value) => value.ToUpper();
+    }
+}
---
+++ fashionREV/Shared/Entities/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fashionREV.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleseDate { get; set; }
+    }
+}
